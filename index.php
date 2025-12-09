762723b9c6667e7b26c5630b16501aa4921bbc4d
git config --global user.email <you@example.com>
2025-12-09 14:51:56 +0100

    inlucdes

---
+++ index.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link rel="stylesheet" href="./assets/css/bootstrap1.min.css" />
+    <script src="./assets/js/feather.min.js"></script>
+</head>
+
+<body>
+
+    <?php
+
+    include 'navbar.html';
+    include 'content.html';
+    include 'footer.html';
+
+    ?>
+
+    <script src="./assets/js/bootstrap.bundle.min.js"></script>
+    <script>feather.replace();</script>
+
+</body>
+
+</html>
