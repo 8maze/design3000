dbfbd8bf789a780181a393da007703245a4ca7a8
git config --global user.email <you@example.com>
2025-12-10 12:13:41 +0100

    mahlzeit

--- index.php
+++ index.php
@@ -6,9 +6,9 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
 
-    <link rel="stylesheet" href="./assets/css/bootstrap.min.css" /> 
+    <link rel="stylesheet" href="./assets/css/bootstrap.min.css" />
     <script src="./assets/js/feather.min.js"></script>
-    
+
 </head>
 
 <body>
@@ -20,8 +20,15 @@
     include 'textbox.html';
     include 'productcards.html';
     include 'bildtext.html';
+    include 'textbox.html';
+    include 'productcards.html';
+    include 'bildtext.html';
+    include 'textbox.html';
+    include 'productcards.html';
+    include 'bildtext.html';
     include 'form.html';
     include 'detail.html';
+
     include 'footer.html';
 
     ?>
